b1d8921a35a927829ad1fe7398f349609dcde204
kgibb8 <kieran.a.gibb@gmail.com>
2016-10-13 20:35:41 +0100

    Styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,11 +11,12 @@ class GamesController < ApplicationController
   end
 
   def create
+    binding.pry
     my_games_params = games_params.to_h
     # my_games_params = my_games_params.merge(creator: current_user.id)
     game = Game.new(my_games_params)
     flash[:notice] = "#{game.name} created successfully!" if game.save
-    render json: game
+    render json: game.to_json
   end
 
   def show
